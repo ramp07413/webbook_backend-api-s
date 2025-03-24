e33f7ea5e04ae29ec041fb0903babf12e222d977
Regarduser <111268408+Regarduser@users.noreply.github.com>
2025-03-24 12:50:09 +0530

    Update sendToken.js
    local host or https

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,8 +1,12 @@
 const sendToken = (user, statuscode, message, res)=>{
     const token = user.generateToken()
+    const isLocalhost = req.headers.origin?.includes("localhost");
+
     res.status(statuscode).cookie("token", token, {
         expires : new Date(Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
-        httpOnly : "true"
+        httpOnly : true,
+        secure: !isLocalhost,  // ✅ Localhost pe false, Production pe true
+           sameSite: isLocalhost ? "Lax" : "None", // ✅ Localhost pe lax, Production pe None
     }).json({
         success : true,
         user,
