4b3316906e0d704428a1ff353dfe6de741a2aab2
Regarduser <111268408+Regarduser@users.noreply.github.com>
2025-03-28 16:42:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('dotenv').config({path : "./config/config.env"})
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const  connectDB  = require("./database/db");
-const  notifyUsers  = require("./services/notifyUsers");
 const {errorMiddleware} = require("./middleware/errorMiddlewares")
 const authRouter = require('./routes/authRouter')
 const bookRouter = require('./routes/bookRouter')
@@ -17,12 +16,15 @@ const removeUnverifiedAccount = require("./services/removeUnverifiedAccount");
 const app = express()
 
 require("dotenv").config();
-// console.log("CLIENT_ID:", process.env.CLIENT_ID || "❌ Missing");
-// console.log("CLIENT_SECRET:", process.env.CLIENT_SECRET || "❌ Missing");
-// console.log("REFRESH_TOKEN:", process.env.REFRESH_TOKEN || "❌ Missing");
-// console.log("FRONTED_URL:", process.env.FRONTEND_URL || "❌ Missing");
 
 
+app.use((err, req, res, next) => {
+    console.error("🔥 ERROR:", err); // ✅ Print full error details
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error",
+    });
+});
 
 app.use(cors({
     origin : [process.env.FRONTEND_URL],
@@ -30,7 +32,7 @@ app.use(cors({
     credentials : true,
 }))
 
-// console.log(process.env.FRONTEND_URL)
+
 
 app.use(cookieParser())
 app.use(express.json())
@@ -47,8 +49,8 @@ app.use('/api/v1/book', bookRouter)
 app.use('/api/v1/borrow', borrowRouter)
 app.use('/api/v1/user', userRouter)
 
-// https://localhost:4000/api/v1/auth
-notifyUsers()
+
+
 removeUnverifiedAccount()
 connectDB()
 app.use(errorMiddleware)
