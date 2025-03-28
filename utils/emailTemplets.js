66394ddfa0ee5b120b0a1d450aa7443f92d7ec90
Regarduser <111268408+Regarduser@users.noreply.github.com>
2025-03-28 16:37:31 +0530

    Add files via upload

--- utils/emailTemplets.js
+++ utils/emailTemplets.js
@@ -1,46 +1,74 @@
 // require('dotenv').config({ path: "./config/config.env" });
 
 function generateVerificationOtpEmailTemplet(otpcode){
-    return ` <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 8px; background-color: #000; color: #fff;">
-    <h2 style="color:#fff; text-align:center;">Verify Your Email Address</h2>
-    <p style="font-size: 16px; color: #ccc;">Dear User,</p>
-    <p style="font-size: 16px; color: #ccc;">To complete your registration or login, please use the following verification code:</p>
-
-    <span style="display: block; font-size: 24px; font-weight: bold; color:#fff ; padding: 10px 20px; border-radius: 5px; background-color: #000; margin: 10px auto; text-align: center;">
-        ${otpcode}
-    </span>
-
-    <p style="font-size: 16px; color: #ccc;">This code is valid for 15 minutes. Please do not share this code with anyone.</p>
-    <p style="font-size: 16px; color: #ccc;">If you did not request this email, please ignore it.</p>
+    return ` 
+<div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 8px; background-color: #000; color: #fff;">
+        <h2 style="color:#fff; text-align:center;">Verify Your Email Address</h2>
+        <p style="font-size: 16px; color: #ccc; text-align: center;">Dear User,</p>
+        <p style="font-size: 16px; color: #ccc; text-align: center;">To complete your registration or login, please use the following verification code:</p>
     
-    <footer style="margin-top: 20px; text-align: center; font-size: 14px; color: #666;">
-        <p>Thank you, <br> BookLab Team</p>
-        <p style="font-size: 12px; color: #444;">This is an automated message. Please don't reply to this email.</p>
-    </footer>
-</div>
+        <span style="display: block; font-size: 24px; font-weight: bold; color:#fff ; padding: 10px 20px; border-radius: 5px; background-color: #000; margin: 10px auto; text-align: center;">
+            ${otpcode}
+        </span>
+    
+        <p style="font-size: 16px; color: #ccc; text-align: center;">This code is valid for 15 minutes. Please do not share this code with anyone.</p>
+        <p style="font-size: 16px; color: #ccc; text-align: center;">If you did not request this email, please ignore it.</p>
+        
+        <footer style="margin-top: 20px; text-align: center; font-size: 14px; color: #666;">
+            <p>Thank you, <br> BookLab Team</p>
+            <p style="font-size: 12px; color: #444;">This is an automated message. Please don't reply to this email.</p>
+        </footer>
+
+    
+    </div>
 
     `
 }
 
 function generateForgotPasswordEmailTemplet(resetpasswordUrl){
-    return ` <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 8px; background-color: #000; color: #fff;">
-    <h2 style="color:#fff; text-align:center;">Verify Your Email Address</h2>
-    <p style="font-size: 16px; color: #ccc;">Dear User,</p>
-    <p style="font-size: 16px; color: #ccc;">To complete your registration or login, please use the following verification code:</p>
+    return ` 
+        <div style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto; padding: 20px; border-radius: 8px; background-color: #000; color: #fff;">
+            <h2 style="color:#fff; text-align:center;">Reset your Booklab Password</h2>
+            <p style="font-size: 16px; color: #ccc; text-align: center;">Dear Booklab User,</p>
+            <p style="font-size: 16px; color: #ccc; text-align: center;">tap reset password button to reset you password:</p>
+        
+            <span style="display: block; font-size: 24px; font-weight: bold; color:#fff ; padding: 10px 20px; border-radius: 5px; background-color: #000; margin: 10px auto; text-align: center;">
+                
+                <a 
+  
 
-    <span style="display: block; font-size: 24px; font-weight: bold; color:#fff ; padding: 10px 20px; border-radius: 5px; background-color: #000; margin: 10px auto; text-align: center;">
-        ${resetpasswordUrl}
-    </span>
 
-    <p style="font-size: 16px; color: #ccc;">This code is valid for 15 minutes. Please do not share this code with anyone.</p>
-    <p style="font-size: 16px; color: #ccc;">If you did not request this email, please ignore it.</p>
-    
-    <footer style="margin-top: 20px; text-align: center; font-size: 14px; color: #666;">
-        <p>Thank you, <br> BookLab Team</p>
-        <p style="font-size: 12px; color: #444;">This is an automated message. Please don't reply to this email.</p>
-    </footer>
-</div>
+  <a 
+  style="text-decoration: none; 
+         padding: 8px 20px; 
+         background-color: #fff; 
+         font-size: 15px; 
+         height: 40px; 
+         width: 150px; 
+         border-top-right-radius: 15px; 
+         border-bottom-left-radius: 15px; 
+         display: inline-block; 
+         text-align: center; 
+         line-height: 40px; 
+         color: #000;"
+  href=${resetpasswordUrl}
+         >
+  Reset Password
+</a>
 
+
+
+            </span>
+            
+        
+            <p style="font-size: 16px; text-align: center; color: #ccc;">This link is valid for 15 minutes.</p>
+            <p style="font-size: 16px; color: #ccc; text-align: center;">If you did not request this email, please ignore it.</p>
+            
+            <footer style="margin-top: 20px; text-align: center; font-size: 14px; color: #666;">
+                <p>Thank you, <br> BookLab Team</p>
+                <p style="font-size: 12px; color: #444;">This is an automated message. Please don't reply to this email.</p>
+            </footer>
+        </div>
     `
 }
 
--- utils/sendToken.js
+++ utils/sendToken.js
@@ -1,6 +1,7 @@
 const sendToken = (user, statuscode, message, res, req)=>{
     const token = user.generateToken()
     const isLocalhost = req.headers.origin && req.headers.origin.includes("localhost");
+
     res.status(statuscode).cookie("token", token, {
         expires : new Date(Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),
         httpOnly : "true",
