2224208bb3d01243c72d75958229386475e9d59e
ramp07413 <ramp07413@gmail.com>
2025-06-02 07:12:44 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ const {isAuthenticated, isAuthorized} = require('../middleware/authMiddleware')
 const router = express.Router()
 
 router.get("/all", isAuthenticated, getAllUsers)
-router.post("/add/newAdmin", isAuthenticated, isAuthorized("Admin"), registerNewAdmin)
+router.get("/add/newAdmin", isAuthenticated, isAuthorized("Admin"), registerNewAdmin)
 router.put("/updateavatar", isAuthenticated, updateAvatar )
 
 module.exports = router
